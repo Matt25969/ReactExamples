4d3171f755ed1f5ac1997b099a7ae25bca9b52a2
Matt25969 <matt.jo.hunt@gmail.com>
2019-05-09 16:28:23 +0100

    Butchered api-conig

--- src/api-config.js
+++ src/api-config.js
@@ -2,4 +2,5 @@ let backendHost;
 
   backendHost = process.env.REACT_APP_SERVICE_HOST;
   
-export const BACKEND_URL = `${backendHost}`;
+//export const BACKEND_URL = `${backendHost}`;
+export const BACKEND_URL = "35.195.31.239";
